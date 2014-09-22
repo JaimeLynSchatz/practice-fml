4976a2559649505ac851c9c15d6de8a2edf67ed5
Jaime Lyn Schatz <jaimelynschatz@gmail.com>
2014-09-22 12:17:46 -0700

    disabling authentication in production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :authenticate_user!
+
+  # disabling requiring authentication as
+  # workaround
+  # before_action :authenticate_user!
 end
