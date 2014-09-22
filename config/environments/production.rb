98cafef33eaf1e233fc63ed4d01e3af8cd0023d8
Jaime Lyn Schatz <jaimelynschatz@gmail.com>
2014-09-22 12:26:10 -0700

    more code swiped from SO

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,11 @@ Rails.application.configure do
   # Configure default url for use in Devise
   # this didn't seem to work config.action_mailer.default_url_options = { :host => 'fixmylist.heroku.com' }
   Rails.application.routes.default_url_options[:host] = 'fixmylist.herokuapp.com'
+  Rails.application.routes.default_url_options[:host] = 'myappsname.herokuapp.com'
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
