0a13f3880c3607e4c7b7efe448fb676fea05b92e
Jaime Lyn Schatz <jaimelynschatz@gmail.com>
2014-09-23 09:54:59 -0700

    require user authentication, was missing Running `rake db:migrate` attached to terminal... up, run.1381 step

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  # disabling requiring authentication as
-  # workaround
-  # before_action :authenticate_user!
+  # breadcrumb: remember to migrate your db!
+  before_action :authenticate_user!
 end
