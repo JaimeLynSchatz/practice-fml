c18d9e05c75e89c64741fd4d6b1fc2d24ad7017d
Jaime Lyn Schatz <jaimelynschatz@gmail.com>
2014-11-21 11:22:00 -0800

    workaround for aws cred issue

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,7 +13,30 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send.
+  # Configure AWS for picture management with Paperclip gem
+  config.paperclip_defaults = {
+    :storage => :s3#,
+#    :s3_credentials => {
+#      :bucket => ENV['S3_BUCKET_NAME'] # TODO set this up
+#      :access_key_id => ENV['AWS_ACCESS_KEY_ID'] # TODO this, too
+#      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'] # TODO this, too
+#    }
+  }
+
+#   #
+#   #
+#   # from: https://devcenter.heroku.com/articles/paperclip-s3
+#   Additionally, we’ll need to the set the AWS configuration variables on the Heroku application.
+# $ heroku config:set S3_BUCKET_NAME=your_bucket_name
+# $ heroku config:set AWS_ACCESS_KEY_ID=your_access_key_id
+# $ heroku config:set AWS_SECRET_ACCESS_KEY=your_secret_access_key
+#
+#
+#
+#
+
+
+    # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
   # Print deprecation notices to the Rails logger.
@@ -29,7 +52,7 @@ Rails.application.configure do
 
   # Confire default urls for use with Devise gem
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-  
+
   # Adds additional error checking when serving assets at runtime.
   # Checks for improperly declared sprockets dependencies.
   # Raises helpful error messages.
